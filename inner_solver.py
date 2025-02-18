be4183ff630dc784617605a852f5acb106a253e4
Andry Monlon <amonlon@protonmail.com>
2025-02-18 11:58:08 +0100

    Added kwargs for scipy solver parametrization

--- inner_solver.py
+++ inner_solver.py
@@ -13,35 +13,44 @@ from numpy import isnan
 from numpy.linalg import norm
 from mb_code.parameters import RTOL, ATOL, MAXITER
 
-def __solver_core(solver, A, b, x0, M):
+def __solver_core(solver, A, b, x0, M, rtol, atol, maxiter):
     iters = 0
     def nonlocal_iterate(xk):
         if isnan(norm(xk)):
             raise ValueError('Linear solver ended due to uncontrolled solution norm. Try to modify the preconditionner or the solver.')
         nonlocal iters
         iters+=1
-    U, info = solver(A, b, x0=x0, M=M, callback=nonlocal_iterate, rtol=RTOL, atol=ATOL, maxiter=MAXITER)
+    U, info = solver(A, b, x0=x0, M=M, callback=nonlocal_iterate, rtol=rtol, atol=atol, maxiter=maxiter)
     return U, info, iters
 
 def scipy_sparse_bicgstab(A, b, **kwargs):
     from scipy.sparse.linalg import bicgstab
     x0 = kwargs.get('x0', None)
     M = kwargs.get('M', None)
-    U, info, iters = __solver_core(bicgstab, A, b, x0, M)
+    rtol = kwargs.get('rtol', RTOL)
+    atol = kwargs.get('atol', ATOL)
+    maxiter = kwargs.get('maxiter', MAXITER)
+    U, info, iters = __solver_core(bicgstab, A, b, x0, M, rtol, atol, maxiter)
     return U, {'info' : info, 'iterations': iters}
 
 def scipy_sparse_cg(A, b, **kwargs):
     from scipy.sparse.linalg import cg
     x0 = kwargs.get('x0', None)
     M = kwargs.get('M', None)
-    U, info, iters = __solver_core(cg, A, b, x0, M)
+    rtol = kwargs.get('rtol', RTOL)
+    atol = kwargs.get('atol', ATOL)
+    maxiter = kwargs.get('maxiter', MAXITER)
+    U, info, iters = __solver_core(cg, A, b, x0, M, rtol, atol, maxiter)
     return U, {'info' : info, 'iterations': iters}
 
 def scipy_sparse_gmres(A, b, **kwargs):
     from scipy.sparse.linalg import gmres
     x0 = kwargs.get('x0', None)
     M = kwargs.get('M', None)
-    U, info, iters = __solver_core(gmres, A, b, x0, M)
+    rtol = kwargs.get('rtol', RTOL)
+    atol = kwargs.get('atol', ATOL)
+    maxiter = kwargs.get('maxiter', MAXITER)
+    U, info, iters = __solver_core(gmres, A, b, x0, M, rtol, atol, maxiter)
     return U, {'info' : info, 'iterations': iters}
 
 def scipy_sparse_spsolve(A, b, **kwargs):
